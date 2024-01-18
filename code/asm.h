e4e5ad5eb46bca37532f72ed6aa2ab8733113234
Thijs Raymakers <thijs@raymakers.nl>
2024-01-18 11:35:40 +0100

    Add inline assembly functions

---
+++ code/asm.h
@@ -0,0 +1,19 @@
+#include <inttypes.h>
+
+static inline __attribute__((always_inline)) void clflush(void* p) {
+	asm volatile("clflush (%0)\n"::"r"(p));
+}
+
+static inline __attribute__((always_inline)) uint64_t rdtsc(void) {
+	uint64_t lo, hi;
+	asm volatile("rdtsc\n" : "=a" (lo), "=d" (hi) :: "rcx");
+	return (hi << 32) | lo;
+}
+
+static inline __attribute__((always_inline)) void lfence() {
+	asm volatile ("lfence\n");
+}
+
+static inline __attribute__((always_inline)) void mfence() {
+	asm volatile ("mfence\n");
+}
