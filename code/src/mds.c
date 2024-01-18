b73175db76816e73518f7ab113a6572ad5907d7f
Thijs Raymakers <thijs@raymakers.nl>
2024-01-18 16:18:45 +0100

    Modify Makefile to be more generic over the files

--- code/src/mds.c
+++ code/src/mds.c
@@ -1,4 +1,12 @@
-int main() {
+#include <stddef.h>
+#include <stdint.h>
+#include "flush_and_reload.h"
 
+int main() {
+  const size_t STRIDE = 4096;
+  const size_t NR_VALUES = 256;
+  uint8_t buffer[STRIDE * NR_VALUES];
+
+  flush(NR_VALUES, STRIDE, buffer);
   return 0;
 }
