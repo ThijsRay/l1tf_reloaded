25d66be870d790e77bfaf9a24447918fb1824bd7
Thijs Raymakers <thijs@raymakers.nl>
2024-01-29 15:08:06 +0100

    Format asm.h

--- code/include/asm.h
+++ code/include/asm.h
@@ -2,37 +2,38 @@
 
 #include <inttypes.h>
 
-static inline __attribute__((always_inline)) void clflush(void* p) {
-	asm volatile("clflush (%0)\n"::"r"(p));
+static inline __attribute__((always_inline)) void clflush(void *p) {
+  asm volatile("clflush (%0)\n" ::"r"(p));
 }
 
 static inline __attribute__((always_inline)) uint64_t rdtsc(void) {
-	uint64_t lo, hi;
-	asm volatile("rdtsc\n" : "=a" (lo), "=d" (hi) :: "rcx");
-	return (hi << 32) | lo;
+  uint64_t lo, hi;
+  asm volatile("rdtsc\n" : "=a"(lo), "=d"(hi)::"rcx");
+  return (hi << 32) | lo;
 }
 
 static inline __attribute__((always_inline)) void lfence() {
-	asm volatile ("lfence\n");
+  asm volatile("lfence\n");
 }
 
 static inline __attribute__((always_inline)) void mfence() {
-	asm volatile ("mfence\n");
+  asm volatile("mfence\n");
 }
 
 static inline __attribute__((always_inline)) void maccess(void *ptr) {
   asm volatile("movq (%0), %%rax\n" : : "r"(ptr) : "rax");
 }
 
-static inline __attribute__((always_inline)) void leak_read(void* leak_buffer, void* reload_buffer) {
+static inline __attribute__((always_inline)) void
+leak_read(void *leak_buffer, void *reload_buffer) {
   asm volatile(
-    // "clflush (%0)\n"
-    // "sfence\n"
-    "movq (%0), %%rax\n"
-    "andq $0xff, %%rax\n"
-    "shlq $0xa, %%rax\n"
-    "prefetcht0 (%1, %%rax)\n"
-    "movq (%1, %%rax), %%rax\n"
-    :: "r" (leak_buffer), "r" (reload_buffer) : "rax"
-  );
+      // "clflush (%0)\n"
+      // "sfence\n"
+      "movq (%0), %%rax\n"
+      "andq $0xff, %%rax\n"
+      "shlq $0xa, %%rax\n"
+      "prefetcht0 (%1, %%rax)\n"
+      "movq (%1, %%rax), %%rax\n" ::"r"(leak_buffer),
+      "r"(reload_buffer)
+      : "rax");
 }
