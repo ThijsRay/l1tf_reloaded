8d2a22667fb264ffff1b20cb455e39d27f9493b1
Thijs Raymakers <thijs@raymakers.nl>
2024-03-21 20:28:02 +0100

    Add proc fs implementation to trigger hypercall

--- src/hypercall/hypercall.c
+++ src/hypercall/hypercall.c
@@ -3,9 +3,15 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/proc_fs.h>
 #include <linux/types.h>
 
 #include <linux/kvm_para.h>
+
+#include "hypercall.h"
+
+#define procfs_name "ipi_hypercall"
+static struct proc_dir_entry *proc_entry;
 
 // Up to four arguments may be passed in rbx, rcx, rdx, and rsi respectively.
 // The hypercall number should be placed in rax and the return value will be
@@ -35,18 +41,46 @@ int send_ipi(unsigned long ipi_bitmap_low, unsigned long ipi_bitmap_high,
   return hypercall(KVM_HC_SEND_IPI, ipi_bitmap_low, ipi_bitmap_high, min, icr);
 }
 
-static int __init hypercall_main(void) {
-  unsigned long mask_low = 0xdeadbeefdeadbeef;
-  unsigned long mask_high = 0xcafebabecafebabe;
-  uint32_t min = 0x12345678;
-  unsigned long icr = 0;
+static ssize_t procfile_write(struct file *file, const char __user *buff,
+                              size_t len, loff_t *off) {
+  int count = 0;
+  struct hypercall_opts opts = {0};
 
-  int count = send_ipi(mask_low, mask_high, min, icr);
-  pr_info("Send to %d CPUs\n", count);
+  // Make sure that only the size of the struct is written
+  if (len != sizeof(opts)) {
+    return -EINVAL;
+  }
+
+  // Copy the buffer from the user
+  if (copy_from_user(&opts, buff, sizeof(opts))) {
+    return -EFAULT;
+  }
+
+  count = send_ipi(opts.mask_low, opts.mask_high, opts.min, opts.icr);
+  pr_info("Sent to IPI %d CPUs\n", count);
+  return count;
+}
+
+static const struct proc_ops proc_file_fops = {
+    .proc_write = procfile_write,
+};
+
+static int __init hypercall_main(void) {
+  proc_entry = proc_create(procfs_name, 0644, NULL, &proc_file_fops);
+  if (NULL == proc_entry) {
+    proc_remove(proc_entry);
+    pr_alert("Error:Could not initialize /proc/%s\n", procfs_name);
+    return -ENOMEM;
+  }
+
+  pr_info("/proc/%s created\n", procfs_name);
   return 0;
 }
 
-static void __exit hypercall_exit(void) { return; }
+static void __exit hypercall_exit(void) {
+  proc_remove(proc_entry);
+  pr_info("/proc/%s removed\n", procfs_name);
+}
 
 module_init(hypercall_main);
 module_exit(hypercall_exit);
