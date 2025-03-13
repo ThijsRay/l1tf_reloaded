a19dc0276c86d617377da9f68b5ce3a12f59a350
mathe <mathe@father>
2025-03-13 20:37:10 +0100

    offsets for father

--- include/reverse.h
+++ include/reverse.h
@@ -9,6 +9,64 @@ typedef unsigned long hva_t; // host virtual address
 typedef unsigned long hpa_t; // host physical address
 typedef unsigned long pte_t; // page table entry - pfn is host physical
 
+#if DEBUG
+
+// struct kvm_apic_map {
+#define MAP_PHYS_MAP	0x218   // struct kvm_lapic *phys_map[max_apic_id+1]
+// };
+
+// struct kvm_lapic {
+#define LAPIC_BASE_ADDR	0x0	// u64 base_address == 0xfee00000
+#define LAPIC_VCPU	0x90	// struct kvm_vcpu *vcpu
+// };
+
+// struct kvm_vcpu {
+#define VCPU_KVM	0x0	// struct kvm *kvm
+#define VCPU_PID	0x78	// struct pid *pid
+#define VCPU_ARCH	0x120	// struct kvm_vcpu_arch arch
+// };
+
+// struct pid {
+#define PID_TASKS	0x20	// struct hlist_head tasks[PIDTYPE_MAX] --> TASK_PID_LINKS
+// };
+
+// struct task_struct {
+#define TASK_TASKS	0x900	// struct list_head tasks
+#define TASK_MM		0x950	// struct mm_struct *mm
+#define TASK_PID	0x9d0	// pid_t pid, tgid
+#define TASK_PID_LINKS	0xa40	// struct hlist_node pid_links[PIDTYPE_MAX] <-- PID_TASKS
+#define TASK_COMM	0xbf0	// char comm[TASK_COMM_LEN]
+// };
+#define TASK_COMM_LEN	0x10
+
+// struct mm_struct {
+#define MM_PGD		0x78	// pgd_t *pgd
+// };
+
+// struct kvm_vcpu_arch {
+#define ARCH_CR3	0xa0	// unsigned long cr3
+#define ARCH_MMU	0x168	// struct kvm_mmu *mmu --> ARCH_ROOT_MMU
+#define ARCH_ROOT_MMU	0x170	// struct kvm_mmu root_mmu
+// }
+
+// struct kvm_mmu {
+#define MMU_ROOT	0x30	// struct kvm_mmu_root_info root;
+// }
+
+// struct kvm_mmu_root_info {
+#define INFO_HPA	0x8	// hpa_t hpa;
+// };
+
+// struct kvm {
+#define KVM_VCPU_ARRAY	0x1128	// struct xarray vcpu_array
+#define KVM_VM_LIST	0x1178	// struct list_head vm_list
+// };
+
+// struct xarray {
+#define XARRAY_HEAD	0x8	// void __rcu *xa_head
+// };
+
+#else // DEBUG
 
 // struct kvm_apic_map {
 #define MAP_PHYS_MAP	0x218   // struct kvm_lapic *phys_map[max_apic_id+1]
@@ -64,6 +122,8 @@ typedef unsigned long pte_t; // page table entry - pfn is host physical
 // struct xarray {
 #define XARRAY_HEAD	0x8	// void __rcu *xa_head
 // };
+
+#endif // DEBUG
 
 
 void get_feeling_for_kernel_kvm_data_structures(void);
